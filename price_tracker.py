67fa3823c02bfa3719d9f855065ea25605bc7d5e
Madarapu Ravichandana <163497310+Chandana-madarapu@users.noreply.github.com>
2026-04-30 19:03:01 +0530

    Update price_tracker.py

--- price_tracker.py
+++ price_tracker.py
@@ -16,7 +16,7 @@ load_dotenv()
 # ----------- LOAD PRODUCTS FROM CSV -----------
 def load_products():
     try:
-        df = pd.read_csv("price_data.csv")
+        df = pd.read_csv("products.csv")
         products = []
 
         for _, row in df.iterrows():
